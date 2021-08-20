40941038be5040f8dfd66baaab6440f949bb1749
Manav Shergill <shergillmanav@uvic.ca>
2021-08-19 17:32:17 -0700

    fixed account names alignment

--- profitly-app/src/components/accountNames/AccountNames.js
+++ profitly-app/src/components/accountNames/AccountNames.js
@@ -1,31 +1,29 @@
-import { Select, Flex } from "@chakra-ui/react";
+import { Select, Text, VStack, HStack } from "@chakra-ui/react";
 import React from "react";
 import "./styles/AccountNames.css";
 export default function AccountNames({ handleChange }) {
   return (
     <div className="account-names-container">
-      <Flex direction="column">
-        <Flex width="100%">
-          <div className="account-names-text">WS Trade Account Type </div>
-          <div className="account-names-dropdown">
-            <Select onChange={handleChange}>
-              <option value={"tfsa"}>TFSA</option>
-              <option value={"rrsp"}>RRSP</option>
-              <option value={"personal"}>PERSONAL</option>
-              <option value={"crypto"}>CRYPTO</option>
-            </Select>
-          </div>
-        </Flex>
-        <br />
-        <Flex>
-          <div className="account-names-text">Questrade Account Type </div>
-          <div className="account-names-dropdown">
-            <Select>
-              <option value={"tfsa"}>TFSA</option>
-            </Select>
-          </div>
-        </Flex>
-      </Flex>
+      <VStack width="100%">
+        <HStack width="50%" justifyContent="center">
+          <Text fontWeight="bold" width="33%">
+            WS Trade Account Type
+          </Text>
+          <Select onChange={handleChange} width="30%">
+            <option value={"tfsa"}>TFSA</option>
+            <option value={"rrsp"}>RRSP</option>
+            <option value={"personal"}>PERSONAL</option>
+          </Select>
+        </HStack>
+        <HStack width="50%" justifyContent="center">
+          <Text fontWeight="bold" width="33%">
+            Questrade Account Type{" "}
+          </Text>
+          <Select width="30%">
+            <option value={"tfsa"}>TFSA</option>
+          </Select>
+        </HStack>
+      </VStack>
     </div>
   );
 }
