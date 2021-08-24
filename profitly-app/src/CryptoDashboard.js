ba69c9d1509d14c2b5007c2c24f13cb480651a67
Manav Shergill <shergillmanav@uvic.ca>
2021-08-24 14:25:00 -0700

    added crypto page, installed jest for testing

--- profitly-app/src/CryptoDashboard.js
+++ profitly-app/src/CryptoDashboard.js
@@ -1,5 +1,22 @@
 import React from "react";
-
+import { Link, Redirect } from "react-router-dom";
 export default function CryptoDashboard() {
-  return <div>Hello Crypto</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        margin: "50px",
+        fontSize: 18,
+        fontWeight: "bold",
+      }}
+    >
+      <h2>Will be available soon</h2>
+      <Link to="/main">
+        <p style={{ color: "blue" }}>{"<"} Return to stocks</p>
+      </Link>
+    </div>
+  );
 }
