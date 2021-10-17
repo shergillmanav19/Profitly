53276dc366df315b204bc132581795b367a40628
Manav Shergill <shergillmanav@uvic.ca>
2021-10-16 22:54:01 -0700

    Updating questrade login mechanism

--- profitly-app/src/pages/StocksAuth.js
+++ profitly-app/src/pages/StocksAuth.js
@@ -2,22 +2,37 @@ import { Button } from "@chakra-ui/button";
 import { VStack } from "@chakra-ui/layout";
 import React, { useEffect } from "react";
 
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 export default function StocksAuth() {
-  const history = useHistory();
+  //ENV VARS
   const qtrade_key = process.env.REACT_APP_QTRADE_API_KEY;
+  const backend = process.env.REACT_APP_BACKEND_URL;
 
+  const history = useHistory();
+
+  //questrade login process
+  let curURL = useLocation();
+  curURL = curURL.search;
+  let code;
+  if (curURL.includes("code")) {
+    code = curURL.match("code=.*$");
+    code = code.toString().split("=")[1];
+    console.log(code);
+  }
+  //-----------------------------------------
   useEffect(() => {
-    fetch("/api/stocks/login")
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Something went wrong");
-        }
-      })
-      .catch((error) => console.log(error));
-  }, []);
+    if (code !== "") {
+      fetch(`${backend}/api/stocks/login/${code}`)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("Something went wrong");
+          }
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [backend, code]);
 
   async function handleLogin() {
     try {
@@ -28,7 +43,7 @@ export default function StocksAuth() {
     <>
       <VStack>
         <a
-          href={`https://login.questrade.com/oauth2/authorize?client_id=${qtrade_key}&response_type=code&redirect_uri=https://localhost:3000/main`}
+          href={`https://login.questrade.com/oauth2/authorize?client_id=${qtrade_key}&response_type=code&redirect_uri=https://auth-profitly-dev.web.app/stocksauth`}
         >
           <Button>Questrade Login</Button>
         </a>
