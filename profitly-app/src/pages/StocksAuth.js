a91dc463548f944158d4bc92837e6169d374a21c
Manav Shergill <shergillmanav@uvic.ca>
2021-10-17 15:37:10 -0700

    Getting questrade auth setup

--- profitly-app/src/pages/StocksAuth.js
+++ profitly-app/src/pages/StocksAuth.js
@@ -1,8 +1,9 @@
 import { Button } from "@chakra-ui/button";
 import { VStack } from "@chakra-ui/layout";
-import React, { useEffect } from "react";
-
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
 import { useHistory, useLocation } from "react-router-dom";
+import Navbar from "../components/navbar/Navbar";
 export default function StocksAuth() {
   //ENV VARS
   const qtrade_key = process.env.REACT_APP_QTRADE_API_KEY;
@@ -20,6 +21,8 @@ export default function StocksAuth() {
     console.log(code);
   }
   //-----------------------------------------
+  const [error, setError] = useState("");
+  const { logout } = useAuth();
   useEffect(() => {
     if (code !== "") {
       fetch(`${backend}/api/stocks/login/${code}`)
@@ -39,12 +42,25 @@ export default function StocksAuth() {
       history.push("/main");
     } catch {}
   }
+  async function handleLogout() {
+    try {
+      setError("");
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("Failed to log out");
+    }
+  }
   return (
     <>
       <VStack>
+        <Navbar handleLogout={handleLogout} />
         <a
           href={`https://login.questrade.com/oauth2/authorize?client_id=${qtrade_key}&response_type=code&redirect_uri=https://auth-profitly-dev.web.app/stocksauth`}
         >
+          {/* <a
+          href={`https://login.questrade.com/oauth2/authorize?client_id=${qtrade_key}&response_type=token&redirect_uri=https://profitly-api.herokuapp.com/api/stocks/login`}
+        > */}
           <Button>Questrade Login</Button>
         </a>
         {/* </a> */}
