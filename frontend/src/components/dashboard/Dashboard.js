d19ab3d6eb7d42f901ccfdbdc14179c3cecb4fd8
Manav Shergill <shergillmanav@uvic.ca>
2021-11-10 21:05:59 -0800

    added a delayed render

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 
 import "./styles/Dashboard.css";
 
@@ -9,6 +9,19 @@ import Watchlist from "../watchlist/Watchlist";
 import AccountNames from "../accountNames/AccountNames";
 
 export default function Dashboard() {
+  const Delayed = ({ children, waitBeforeShow = 1000 }) => {
+    const [isShown, setIsShown] = useState(false);
+
+    useEffect(() => {
+      console.log(waitBeforeShow);
+      setTimeout(() => {
+        setIsShown(true);
+      }, waitBeforeShow);
+    }, [waitBeforeShow]);
+
+    return isShown ? children : null;
+  };
+
   const [accountName, setAccountName] = useState("tfsa");
 
   const handleAccountNameChange = (e) => {
@@ -24,8 +37,10 @@ export default function Dashboard() {
       </div>
       <div>
         <div className="wtch-pos-container">
-          <PortfolioPositions accountName={accountName} />
-          <Watchlist />
+          <Delayed>
+            <PortfolioPositions accountName={accountName} />
+            <Watchlist />
+          </Delayed>
         </div>
       </div>
     </>
